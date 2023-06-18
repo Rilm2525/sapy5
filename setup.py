75faacd9d7fca23ace2a35556b344a91de2e8039
Rilm2525 <61257810+Rilm2525@users.noreply.github.com>
2023-06-19 04:22:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ setup(
     version='0.0.0.2',
     license='MIT',
     description='Makes it easy to use sapi from Python.',
-
+    
     author='Rilm2525',
     author_email='rilm2525ce@gmail.com',
     url='https://twitter.com/Rilm2525',
-
+    
+    install_requires=[
+        "pywin32",
+        "Wave",
+    ],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
 )
